4cb1c2bd7963ebc58732213339c888a7ee3c33aa
Lovitra Mehta <126038901+LovitraMehta@users.noreply.github.com>
2025-07-15 20:59:08 +0530

    Time: 1 ms (99.12%), Space: 42.1 MB (62.25%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String word) 
+    {
+        if (word.length()<3) return false;
+        boolean isVowel=false;
+        boolean isConsonant=false;
+        for (char c: word.toCharArray())
+        {
+            if (Character.isLetter(c))
+            {
+            char ch=Character.toLowerCase(c);
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+            isVowel=true;
+            else
+            isConsonant=true;
+            }
+            else if (!Character.isDigit(c))
+            return false;
+        }
+        return (isVowel && isConsonant);
+    }
+}
